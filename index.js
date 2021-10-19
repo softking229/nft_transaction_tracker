a09c2a559661b839be08694323c568bcf88626e0
softking229 <softking229@outlook.com>
2021-10-19 02:32:05 -0500

    added error handling in mongoose

--- index.js
+++ index.js
@@ -13,15 +13,12 @@ var timerEvent = function() {
     getTransferList();
 };
 
-//Set up default mongoose connection
-var mongoDB = 'mongodb://localhost:27017';
-mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
-
-//Get the default connection
-var db = mongoose.connection;
-
-//Bind connection to error event (to get notification of connection errors)
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+try {
+    await mongoose.connect(process.env.DB_PATH, {useNewUrlParser: true, useUnifiedTopology: true});
+} catch (err) {
+    console.error("Please check MongoDB connection");
+    process.exit();
+}
 
 setInterval(timerEvent, process.env.INTERVAL_DELAY);
 
